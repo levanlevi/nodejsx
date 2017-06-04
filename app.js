c86873db9f686c39dc78be0ca441478e5460a410
Levan Khunjgurua <l.khunjgurua@gmail.com>
2017-06-04 19:31:14 +0200

    Book Verbs

--- app.js
+++ app.js
@@ -14,43 +14,15 @@ var port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+var bookRouter = require('./routes/bookRoutes')(Book);
+//var authorRouter = require('./routes/authorRoutes')();
 
-bookRouter.route('/Books')
-    .post(function (req, res) {
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function (req, res) {
-        var query = {};
-        if (req.query.genre) {
-            query = req.query.genre;
-        }
-        Book.find(query, function (err, books) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Book/:bookId')
-    .get(function (req, res) {
-        Book.findById(req.params.bookId, function (err, book) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.send(book);
-        })
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', function (req, res) {
     res.send('welcome to my api!');
 });
 
 app.listen(port, function () {
-    console.log('application is running on port' + port);
+    console.log('application is running on port: ' + port);
 });
